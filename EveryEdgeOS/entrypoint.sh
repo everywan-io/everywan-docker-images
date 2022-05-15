7b6eef6e52549e3344d2a20230a8ab6d889ee111
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-05-15 17:28:05 +0200

    Add missing ethtool dependency and disable controller tx/rx offload to solve communications issues related to VXLAN tunnels

--- EveryEdgeOS/entrypoint.sh
+++ EveryEdgeOS/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Disable offload to fix communication issues coming when the controller sends VXLAN traffic to the EveryEdge
+ethtool -K eth0 tx off rx off
+
 etherws sw
 python3 -m srv6_sdn_control_plane.srv6_controller -c /configs/config.ini
